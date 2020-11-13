32f2cc77a670bfb36d0a695a16efab6bb8ae44b7
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-11-13 09:55:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Random numero = new Random();
+    int aleatorio = numero.Next(0, 20);
+    int intentos = 0;
+    Console.WriteLine ("Ingresa un número entre el 0 y el 20");
+    int minumero = int.Parse(Console.ReadLine());
+    while(numero !=)
+  }
+}
